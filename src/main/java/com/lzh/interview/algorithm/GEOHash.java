69a39c708803f06d3f6dbb1da8d9f087da0757f0
lizhuohang <li_zhuohang@126.com>
2016-12-20 20:06:35 +0800

    add static threadlocal

---
+++ src/main/java/com/lzh/interview/algorithm/GEOHash.java
@@ -0,0 +1,7 @@
+package com.lzh.interview.algorithm;
+
+/**
+ * Created by lizhuohang on 2016/12/20.
+ */
+public class GEOHash {
+}
---
+++ src/main/java/com/lzh/interview/multithread/StaticThreadLocal.java
@@ -0,0 +1,70 @@
+package com.lzh.interview.multithread;
+
+/**
+ * Created by lizhuohang on 2016/12/20.
+ *
+ * ThreadLocal with static : variables dependent on thread
+ *
+ * static variables is quoted by every instances in one thread,separated within different threads
+ */
+public class StaticThreadLocal {
+
+    /**
+     * 1
+     * @param args
+     */
+    private static ThreadLocal<Integer> seqNum = new ThreadLocal<Integer>(){
+        public Integer initialValue(){
+            return 0;
+        }
+    };
+
+    public int getNexNum(){
+        seqNum.set((seqNum.get()) + 1);
+        return seqNum.get();
+    }
+
+
+    /**
+     * 2
+     * @param args
+     */
+//    private static int a = 0;
+//    public synchronized int getNexNum(){
+//        return ++a;
+//    }
+
+    public static void main(String[] args) {
+        StaticThreadLocal st = new StaticThreadLocal();
+        TestClient t1 = new TestClient(st);
+        TestClient t2 = new TestClient(st);
+        TestClient t3 = new TestClient(st);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.print();
+        t2.print();
+        t3.print();
+    }
+
+    private static class TestClient extends Thread{
+        private StaticThreadLocal st ;
+        public TestClient(StaticThreadLocal st){
+            this.st = st;
+        }
+
+        public void run(){
+            for(int i = 0 ; i<3 ; i++) {
+                System.out.println(Thread.currentThread().getName() + " --> " + st.getNexNum());
+            }
+        }
+
+        public void print(){
+            for(int i = 0 ; i<3 ; i++) {
+                System.out.println(Thread.currentThread().getName() + " --> " + st.getNexNum());
+            }
+        }
+    }
+}
